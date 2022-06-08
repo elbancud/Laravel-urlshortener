c56e6c4d2475cb07136d44ae60415df4ab76bab0
emarson <elbancud@gmail.com>
2022-06-08 14:26:09 +0800

    modified: store initialized

--- vue/src/store/index.ts
+++ vue/src/store/index.ts
@@ -1,10 +1,15 @@
 import { createStore } from "vuex";
 
 const store = createStore({
-    state: {},
+    state: {
+        data: {
+            name: "raffy",
+        },
+    },
     getters: {},
     setters: {},
-    module: {},
+    modules: {},
     actions: {},
     mutations: {},
 });
+export default store;
