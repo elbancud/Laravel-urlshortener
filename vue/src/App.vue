b35b25d40e7f75cfcba3b83920c1985015ee2b1d
emarson <elbancud@gmail.com>
2022-06-09 08:08:20 +0800

    modified: views formatted

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import index from "./store/index";
-</script>
-
+<script setup lang="ts"></script>
 <template>
     <router-view></router-view>
 </template>
--- vue/src/axios/index.ts
+++ vue/src/axios/index.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
 // import router from "./router";
 
-const axiosClient = axios.create({});
+const axiosClient = axios.create({
+    baseURL: "",
+});
