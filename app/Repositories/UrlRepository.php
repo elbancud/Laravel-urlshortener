b1255540c9a1329843d5d5269ac4f4ccd95ca59d
emarson <elbancud@gmail.com>
2022-06-09 09:44:58 +0800

    chore: request and interfaces initialized

--- app/Repositories/UrlRepository.php
+++ app/Repositories/UrlRepository.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace App\Repository\UrlRepository;
+
+use App\Interfaces\UrlInterface;
+use Illuminate\Database\Model;
+
+class UrlRepository implements UrlInterface {
+    protected $model;
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function saveUrl() {
+
+    }
+} 
