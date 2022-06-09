7c61d8236d39a76a0152ae1e5b653013617b93a8
emarson <elbancud@gmail.com>
2022-06-09 08:03:28 +0800

    modified: app router view and home initialized

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,16 +5,5 @@ import index from "./store/index";
 </script>
 
 <template>
-    <div>sds</div>
+    <router-view></router-view>
 </template>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
--- vue/src/routes/index.ts
+++ vue/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../components/Home";
+import Home from "../components/Home.vue";
 
-const routes = [
-    path: "/",
-    component: Home,
-];
+const routes = [{ path: "/", component: Home }];
 
 const router = createRouter({
-    history: createWebHistory,
+    history: createWebHistory(),
     routes,
 });
+// router.beforeEach((to, from, next) => {
+
+// });
+
+export default router;
